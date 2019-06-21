9214278cbb43bd3a961b367ad8fc0733eb496ac0
iyoungman <hsl9546@gmail.com>
2019-06-22 04:25:43 +0900

    git init

---
+++ src/main/java/com/youngman/mopbatch/MopBatchApplication.java
@@ -0,0 +1,13 @@
+package com.youngman.mopbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MopBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MopBatchApplication.class, args);
+	}
+
+}
