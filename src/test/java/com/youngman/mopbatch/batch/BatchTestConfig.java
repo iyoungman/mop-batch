3ee36376067cdab1dd92b8d101b9754684916648
iyoungman <hsl9546@gmail.com>
2019-10-25 02:40:33 +0900

    배치도 젠킨스 연동2

--- src/test/java/com/youngman/mopbatch/batch/BatchTestConfig.java
+++ src/test/java/com/youngman/mopbatch/batch/BatchTestConfig.java
@@ -1,27 +1,27 @@
-package com.youngman.mopbatch.batch;
-
-import org.springframework.batch.core.Job;
-import org.springframework.batch.test.JobLauncherTestUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * Created by YoungMan on 2019-07-04.
- */
-
-@Configuration
-public class BatchTestConfig {
-
-	@Autowired
-	@Qualifier(value = "clubDailyStatisticsJob")
-	private Job clubDailyStatisticsJob;
-
-	@Bean
-	public JobLauncherTestUtils jobLauncherTestUtils() {
-		JobLauncherTestUtils jobLauncherTestUtils = new JobLauncherTestUtils();
-//		jobLauncherTestUtils.setJob(clubDailyStatisticsJob);
-		return jobLauncherTestUtils;
-	}
-}
+//package com.youngman.mopbatch.batch;
+//
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.test.JobLauncherTestUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * Created by YoungMan on 2019-07-04.
+// */
+//
+//@Configuration
+//public class BatchTestConfig {
+//
+//	@Autowired
+//	@Qualifier(value = "clubDailyStatisticsJob")
+//	private Job clubDailyStatisticsJob;
+//
+//	@Bean
+//	public JobLauncherTestUtils jobLauncherTestUtils() {
+//		JobLauncherTestUtils jobLauncherTestUtils = new JobLauncherTestUtils();
+////		jobLauncherTestUtils.setJob(clubDailyStatisticsJob);
+//		return jobLauncherTestUtils;
+//	}
+//}
--- src/test/java/com/youngman/mopbatch/domain/member/dao/MemberFindDaoTest.java
+++ src/test/java/com/youngman/mopbatch/domain/member/dao/MemberFindDaoTest.java
@@ -1,40 +1,40 @@
-package com.youngman.mopbatch.domain.member.dao;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.notNullValue;
-import static org.junit.Assert.assertThat;
-
-/**
- * Created by YoungMan on 2019-06-27.
- */
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class MemberFindDaoTest {
-
-	@Autowired
-	private MemberFindDao memberFindDao;
-
-
-	@Test
-	public void findFcmTokensByChairEmails() {
-
-		//given
-		List<String> emails = Arrays.asList("test", "test2");
-
-		//when
-		List<String> fcmTokens = memberFindDao.findFcmTokensByEmails(emails);
-
-		//then
-		assertThat(fcmTokens, is(notNullValue()));
-	}
-}
+//package com.youngman.mopbatch.domain.member.dao;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//import static org.hamcrest.core.Is.is;
+//import static org.hamcrest.core.IsNull.notNullValue;
+//import static org.junit.Assert.assertThat;
+//
+///**
+// * Created by YoungMan on 2019-06-27.
+// */
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class MemberFindDaoTest {
+//
+//	@Autowired
+//	private MemberFindDao memberFindDao;
+//
+//
+//	@Test
+//	public void findFcmTokensByChairEmails() {
+//
+//		//given
+//		List<String> emails = Arrays.asList("test", "test2");
+//
+//		//when
+//		List<String> fcmTokens = memberFindDao.findFcmTokensByEmails(emails);
+//
+//		//then
+//		assertThat(fcmTokens, is(notNullValue()));
+//	}
+//}
